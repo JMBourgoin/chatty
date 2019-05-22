591666db5121efd7165aee997d3dd50629df278a
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-22 11:42:38 -0400

    add rails auth pattern

---
+++ app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  username        :string           not null
+#  email           :string
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+    validates :username, :session_token, :password_digest, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    attr_reader :password
+    after_initialize :ensure_session_token
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.update!(session_token: self.class.generate_session_token)
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def self.find_by_credentials(username, password)
+    end
+
+    def is_password?(password)
+        bcrypt_password = BCrypt::Password.new(self.password_digest)
+        bcrypt_password.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64
+    end
+
+end
