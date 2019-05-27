2766b84666c8412a80d4e711bddcc5c2a14030e2
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-27 11:51:47 -0400

    add root & app.jsx

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <Route exact path="/" component={}/>
+        </div>
+    )
+};
