90ea322a4fc52c3c73177e6125a16a10c64a61bb
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-27 12:18:22 -0400

    edit application controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,21 @@
 class Api::SessionsController < ApplicationController
     def create
         @user = User.find_by_credentials(params[:user][:username], [:user][:password])
+        if @user
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: ["Invalid email/password"], status: 403
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user
+            logout!
+            render "api/users/show"
+        else
+            render json: ["There is no user currently signed in"], status: 404
+        end
     end
 end
