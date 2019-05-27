a3b6c426655e0835efd832c2f00c31a3b994f98c
Jason Bourgoin <jason@jmbourgoin.com>
2019-05-27 11:34:15 -0400

    add jbuilder views for users api & bootstrap user

--- frontend/chatty.jsx
+++ frontend/chatty.jsx
@@ -3,4 +3,20 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
+    // if (window.currentUser) {
+    //     const preloadedState = {
+    //         session: { currentUserId: window.currentUser.id },
+    //         entities: {
+    //             users: { [window.currentUser.id]: window.currentUser }
+    //         }
+    // };
+    //     store = configureStore(preloadedState);
+    //     delete window.currentUser;
+    // } else {
+    //     store = configureStore();
+    // }
+    window.getState = store.getState();
+
+    const root = document.getElementById('root');
+    ReactDOM.render(<Root store={ store }/>, root);
 });
